1cc0339d3c248dff120d7094ecc9616f3617e2e3
dostonkomilov7 <doston.komilov7@icloud.com>
2026-07-10 20:14:15 +0500

    video-file

--- src/files/validation/files-validation.ts
+++ src/files/validation/files-validation.ts
@@ -12,39 +12,39 @@ export class FilesValidation implements PipeTransform {
 
   private readonly mimeTypes: Record<ValidateFileOptions['type'], RegExp> = {
     image: /(png|jpg|jpeg)$/,
-    video: /(avi|mp4|mpeg|mov|wmv)$/,
+    video: /(avi|mp4|mpeg|mov|wmv|quicktime|x-msvideo|x-ms-wmv)$/,
   };
 
   constructor(options: ValidateFilesOptions) {
     this.options = options;
   }
 
-  private validateFiles(files: Array<Express.Multer.File>) {
+  private validateFiles(fieldName: string, files: Array<Express.Multer.File>) {
+    const validation = this.options?.[fieldName];
+    if (!validation?.type) return;
+
     for (const file of files) {
-      const validation = this.options?.[file.fieldname];
-      if (validation?.type) {
-        const mime = file.mimetype.split('/')[1];
-        const mimeRegex = this.mimeTypes[validation.type];
-        if (!mimeRegex.test(mime)) {
-          throw new HttpException(
-            `Invalid file type! Only ${validation.type} files are allowed`,
-            HttpStatus.UNSUPPORTED_MEDIA_TYPE,
-          );
-        }
+      const mime = file.mimetype.split('/')[1];
+      const mimeRegex = this.mimeTypes[validation.type];
+      if (!mimeRegex.test(mime)) {
+        throw new HttpException(
+          `Invalid file type for "${fieldName}"! Only ${validation.type} files are allowed`,
+          HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+        );
       }
     }
   }
 
   transform(value: Record<string, Array<Express.Multer.File>>) {
     for (const fieldName of Object.keys(value)) {
-      const files: Array<Express.Multer.File> = [...value?.[fieldName]];
+      const files: Array<Express.Multer.File> = [...(value?.[fieldName] ?? [])];
       if (this.options?.[fieldName]?.required && !files?.length) {
         throw new HttpException(
           `The field ${fieldName} should contain at least one file`,
           HttpStatus.LENGTH_REQUIRED,
         );
       }
-      this.validateFiles(files);
+      this.validateFiles(fieldName, files);
     }
     return value;
   }
